fac05c25399deaaa5e852272bf77cf201f0fb54f
tauanmiguel <tml@priceshape.dk>
2026-03-12 09:25:16 +0100

    Add split keybindings

--- lua/tauan/remap.lua
+++ lua/tauan/remap.lua
@@ -4,3 +4,6 @@ vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 vim.keymap.set("n", "<leader>kb", function()
   vim.cmd("Lex 20")
 end)
+
+vim.keymap.set("n", "<leader>vs", vim.cmd.vsplit)
+vim.keymap.set("n", "<leader>hs", vim.cmd.split)
